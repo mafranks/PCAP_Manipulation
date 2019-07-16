229ebfbe96be949a302a1629defdb63c8674938a
Matthew Franks <42283335+mafranks@users.noreply.github.com>
2019-07-16 10:04:49 -0400

    Update and rename pcap_splitter_multi.py to pcap_splitter.py

--- pcap_splitter.py
+++ pcap_splitter.py
@@ -4,18 +4,16 @@
 
 import os
 import sys
-import scapy.all
-import multiprocessing
 from datetime import datetime
+import scapy.all
 
 startTime = datetime.now()
 currentDirectory = '/Users/mafranks/Desktop/National Guard/WOAC'
-filename = 'Practical_Application.pcap'
+filename = 'final201.pcap'
 targetDirectory = f"splitPCAPs-{filename.split('.')[0]}"
+IPCount = 0
+etherCount = 0
 
-def process_packet(pcap):
-    for packet in pcap[0]:
-        print(packet)
 
 print('[+] Creating output directory')
 if not os.path.exists(f'{currentDirectory}/{targetDirectory}'):
@@ -26,15 +24,28 @@ else:
     sys.exit(f'[-] Please delete {currentDirectory}/{targetDirectory} and try again')
 
 print('[+] Loading PCAP (be patient)')
+sTime = datetime.now()
 try:
     pcap = scapy.all.rdpcap(f'{currentDirectory}/{filename}')
+
 except Exception as e:
     sys.exit(f'[-] Encountered error: {e}')
+eTime = datetime.now()
+print(f'[+] It took {eTime-sTime} to load the pcap')
 
 print('[+] Splitting into separate PCAPs')
-pool = multiprocessing.Pool()
-pool.map(process_packet, pcap)
+for packet in pcap:
+    try:
+        scapy.all.wrpcap(f'{currentDirectory}/{targetDirectory}/{packet.payload.src}.pcap', packet, append=True)  
+        scapy.all.wrpcap(f'{currentDirectory}/{targetDirectory}/{packet.payload.dst}.pcap', packet, append=True)
+        IPCount += 1
+    except:
+        etherCount += 1
 
 print('[+] Processing complete')
+print(f'[+] Total packets: {IPCount + etherCount}')
+print(f'[+] IP packets: {IPCount}')
+print(f'[+] Ether packets: {etherCount}')
+
 endTime = datetime.now()
 print(f'[+] Processing time {endTime-startTime}')
